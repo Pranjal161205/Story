cf05985c34f99bea75a9d01d111010705dd08bbd
Pranjal161205 <79497793+Pranjal161205@users.noreply.github.com>
2021-10-06 14:00:37 +0530

    Add files via upload

---
+++ Screens/Feed.js
@@ -0,0 +1,12 @@
+import React,{Component, Components} from 'react';
+import { Text,View } from "react-native";
+
+export default class CreateStory extends Component{
+    render(){
+        return(
+            <View style = {{flex: 1}}>
+                <Text> Feed </Text>
+            </View>
+        )
+    }
+}
